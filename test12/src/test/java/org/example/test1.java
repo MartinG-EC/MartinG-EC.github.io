c1241dd25d216fd31254575dc7da7db4fa6e83fd
Martin Gustafsson <74515162+MartinG-EC@users.noreply.github.com>
2024-11-24 22:09:50 +0100

    Add files via upload

---
+++ test12/src/test/java/org/example/test1.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class test1 {
+
+    @Test
+    void testAnalyseraText_EnRad() {
+    }
+
+    @Test
+    void testAnalyseraText_FleraRader() {
+    }
+
+    @Test
+    void testAnalyseraText_StopRad() {
+    }
+
+    @Test
+    void testAnalyseraText_EmptyText() {
+    }
+}
